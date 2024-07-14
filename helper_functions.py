295fc94e17b1786a13de9a382336e082cf73e79a
adamoah <100427408+adamoah@users.noreply.github.com>
2024-07-14 18:04:20 -0400

    Update helper_functions.py

--- helper_functions.py
+++ helper_functions.py
@@ -1,5 +1,7 @@
 import math
 import numpy as np
+import torch.nn as nn
+import torch
 
 def split(ids, train, val, test):
     # proportions of train, val, test
@@ -17,3 +19,19 @@ def split(ids, train, val, test):
     test = np.where(np.isin(ids, IDs[train_split+val_split:]))[0]
     
     return train, val, test
+
+def compute_distil_loss(t_outputs, s_outputs, targets, s_loss_fn, KLD, temperature=20, lambda_param=0.9):
+    # compute soft_targets
+    soft_teacher = nn.functional.softmax(t_outputs.squeeze() / temperature, dim=-1)
+    soft_student = nn.functional.log_softmax(s_outputs.squeeze() / temperature, dim=-1)
+
+    # Compute distillation loss
+    distillation_loss = KLD(soft_student, soft_teacher) * (temperature**2)
+
+    # true label loss
+    student_target_loss = s_loss_gn(student_outputs.squeeze(), targets.squeeze())
+
+    # return final distillation loss
+    return (1. - lambda_param) * student_target_loss + lambda_param * distillation_loss
+    
+    
